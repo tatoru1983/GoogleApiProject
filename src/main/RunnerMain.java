9b4c3181b0a194c69f9df301d1f411a8664e5813
tatoru1983 <37672216+tatoru1983@users.noreply.github.com>
2019-01-15 23:41:00 +0100

    main class

---
+++ src/main/RunnerMain.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class RunnerMain {
+
+	public static void main(String[] args) throws IOException, GeneralSecurityException {
+		String step = args[0];
+		String[] argsStep = {args[1]};
+		
+		switch(step) {
+		case "CASH_COLLECT":
+			RunnerCashCollect.main(argsStep);
+			break;
+		case "SELF_REPORT":
+			RunnerSelfReport.main(argsStep);
+			break;
+		case "ITEM_LIST":
+			RunnerItemList.main(argsStep);
+			break;
+		}
+	}
+
+}
